d7172c0f636b5682fc128c17ac71d183cf6545d6
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-16 19:55:19 -0300

    feat: user types table creation migration

---
+++ src/database/migrations/1645040851839-CreateUserTypes.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryResult, QueryRunner, Table } from "typeorm";
+
+export class CreateUserTypes1645040851839 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'user_types',
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        isUnique: true,
+                        generationStrategy: "uuid",
+                        default: "uuid_generate_v4()",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        isNullable: false,
+                    },
+                ],
+            }
+            )
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable('user_types')
+    }
+
+}
