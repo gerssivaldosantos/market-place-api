f920c411919faaf7d3d1cf2f6ba2a5ef741fac8d
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-16 16:20:11 -0300

    feat: routes connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes)
 
 app.listen(8085, () => console.log("Server started on port 8085"));
