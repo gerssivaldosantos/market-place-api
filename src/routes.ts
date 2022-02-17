6da1bcc92f47f9b362b79b675b48125af48e2504
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-16 22:20:20 -0300

    feat: route to get all users

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import UserController from "./controllers/UserController";
 
 /* basic router for hello world */
 
 const routes = Router();
 
-routes.get('/', (req, res) => {res.send("Hello World!");});
+routes.get('/', (req, res) => { res.send("Hello World!"); });
+routes.get('/users', UserController.getAll);
 
 export default routes;
