da18eacc470abf989f910389f437031e7fe8ca53
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-17 00:14:12 -0300

    feat: uuid generation

--- src/entities/UserTypeEntity.ts
+++ src/entities/UserTypeEntity.ts
@@ -1,9 +1,13 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
-
+import { BeforeInsert, Column, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 @Entity('user_types')
 export default class UserType{
     @PrimaryColumn()
     id: string;
+    @BeforeInsert()
+    insertId(){
+        this.id = uuid();
+    }
     @Column()
     name: string;
     @Column()
