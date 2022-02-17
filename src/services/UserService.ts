9e232b7d1ddfafaaef6eb0f668ae3f0eab06d5d6
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-17 01:18:52 -0300

    feat: user creation

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,18 +1,57 @@
-import { getRepository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
+import { UserRequest } from "../@types/userRequest";
 import { User } from "../entities/UserEntity";
 
 class UserService {
+
+    async create(user_request: UserRequest) {
+        try {
+            const repository = getRepository(User);
+            const { name, user_type_id, email, password } = user_request;
+            const user = repository.create({
+                name,
+                user_type_id,
+                email,
+                password
+            });
+            await repository.save(user);
+            return {
+                status: 201,
+                message: "User created successfully",
+                data: user
+            };
+        } catch (err) {
+            return {
+                status: 500,
+                message: "Internal Server Error",
+                data: err
+            };
+        }
+    }
+
     async getAll() {
-        try{
+        try {
             const repository = await getRepository(User)
             const users = repository.find();
             if (!users) {
-                return undefined;
+                return {
+                    status:404,
+                    message: "Users not found",
+                    content: null
+                };
             }
-            return users;
+            return {
+                status: 200,
+                message: "Users found",
+                content: users
+            };
         }
-        catch{
-            return undefined;
+        catch {
+            return {
+                status: 500,
+                message: "Internal Server Error",
+                content: null
+            };
         }
     }
 }
