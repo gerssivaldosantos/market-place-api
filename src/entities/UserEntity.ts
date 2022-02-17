a79b8a8fddde10212c38fdd07b7bfc568752d458
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-17 01:19:26 -0300

    feat: user type id setting

--- src/entities/UserEntity.ts
+++ src/entities/UserEntity.ts
@@ -1,17 +1,24 @@
-import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn, BeforeInsert, BeforeUpdate} from "typeorm";
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn, BeforeInsert, BeforeUpdate, ManyToOne, JoinColumn} from "typeorm";
 import {v4 as uuid} from 'uuid';
 import bcrypt from 'bcryptjs';
+import UserType from "./UserTypeEntity";
 
 @Entity('users')
 export class User {
 
     @PrimaryColumn()
     id: string;
-
     @BeforeInsert()
-    async insertId(){
-    this.id = uuid();
+    async generateId(){
+        this.id = uuid();
     }
+    @Column()
+    user_type_id: string;
+
+    @ManyToOne(()=> UserType)
+    @JoinColumn({name:"user_type_id"})
+    user_type: UserType;
+    
     @Column()
     name: string;
 
@@ -30,6 +37,7 @@ export class User {
     @BeforeInsert()
     @BeforeUpdate()
     async hashPassword(){
+        
         this.password = await bcrypt.hash(this.password, 8);
         this.updated_at = new Date();
     }
