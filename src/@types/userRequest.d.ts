5e37850fe18a9241da94ffa817709887e0489634
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-19 14:32:50 -0300

    feat: validate middleware structure

--- src/@types/userRequest.d.ts
+++ src/@types/userRequest.d.ts
@@ -1,6 +1,13 @@
-export type UserRequest = {
+import { Contains, isEmail, isUUID, Length } from 'class-validator';
+
+export class UserRequest{
+    @Length(3, 60)
     name: string;
+    @isUUID()
     user_type_id: string;
+    @isEmail()
     email: string;
+    @Length(6, 60)
+    @Contains(number, lowercase, uppercase, special)
     password: string;
 }
