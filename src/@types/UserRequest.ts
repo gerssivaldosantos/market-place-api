9a06b468fe3eafb9ca98b318392e909071dd06ea
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-19 16:35:36 -0300

    feat: just validate if the value is different of undefined

--- src/@types/UserRequest.ts
+++ src/@types/UserRequest.ts
@@ -1,13 +1,28 @@
-import { IsEmail, IsString, IsUUID, Length } from 'class-validator';
+import { IsEmail, IsString, IsUUID, Length, ValidateIf } from 'class-validator';
 
 export class UserRequest{
+
+    @ValidateIf(o => o.otherProperty !== undefined)
     @Length(3, 60)
     name: string;
+
+    @ValidateIf(o => o.otherProperty !== undefined)
     @IsString()
     @IsUUID()
     user_type_id: string;
+
+    @ValidateIf(o => o.otherProperty !== undefined)
     @IsEmail()
     email: string;
+
+    @ValidateIf(o => o.otherProperty !== undefined)
     @Length(6, 60)
     password: string;
+
+    constructor({name, user_type_id, email, password}){
+        this.name = name;
+        this.user_type_id = user_type_id;
+        this.email = email;
+        this.password = password;
+    }
 }
