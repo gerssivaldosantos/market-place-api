300368c770a3b30a9ac7a8c17a050f84f26467c8
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-19 15:45:26 -0300

    fix: rename and instance

--- src/middlewares/RequestValidator.ts
+++ src/middlewares/RequestValidator.ts
@@ -1,20 +1,27 @@
 import { validate } from 'class-validator';
-import { UserRequest } from '../@types/userRequest';
+import { UserRequest } from '../@types/UserRequest';
 import { Request, Response, NextFunction} from 'express';
 
 class RequestValidator {
 
     async user(req: Request, res: Response, next: NextFunction) {
         const user = req.body as UserRequest;
-        const errors = await validate(user);
+        const user_request = new UserRequest();
+        user_request.name = user.name;
+        user_request.email = user.email;
+        user_request.password = user.password;
+        user_request.user_type_id = user.user_type_id;
+        const errors = await validate(user_request);
         if (errors.length > 0) {
-            return {
-                status: 400,
+            return res.status(400).json({
                 message: 'Bad Request',
-                content: errors
-            };
+                content: errors[0].constraints ?? null
+            });
 
         }
         next();
     }
 }
+
+
+export default new RequestValidator();
