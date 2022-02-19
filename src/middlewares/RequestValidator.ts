f3305134f6bbb386dce360c01bda56f98a76ebb7
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-19 16:36:19 -0300

    refactor: new method to instance the user request

--- src/middlewares/RequestValidator.ts
+++ src/middlewares/RequestValidator.ts
@@ -6,12 +6,9 @@ class RequestValidator {
 
     async user(req: Request, res: Response, next: NextFunction) {
         const user = req.body as UserRequest;
-        const user_request = new UserRequest();
-        user_request.name = user.name;
-        user_request.email = user.email;
-        user_request.password = user.password;
-        user_request.user_type_id = user.user_type_id;
+        const user_request = new UserRequest(user);
         const errors = await validate(user_request);
+
         if (errors.length > 0) {
             return res.status(400).json({
                 message: 'Bad Request',
