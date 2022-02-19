0e724166cc7582b3ce6d75840f7f21bddc2f5b69
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-19 15:45:08 -0300

    feat: rename

--- src/@types/UserRequest.ts
+++ src/@types/UserRequest.ts
@@ -1,13 +1,13 @@
-import { Contains, isEmail, isUUID, Length } from 'class-validator';
+import { IsEmail, IsString, IsUUID, Length } from 'class-validator';
 
 export class UserRequest{
     @Length(3, 60)
     name: string;
-    @isUUID()
+    @IsString()
+    @IsUUID()
     user_type_id: string;
-    @isEmail()
+    @IsEmail()
     email: string;
     @Length(6, 60)
-    @Contains(number, lowercase, uppercase, special)
     password: string;
 }
