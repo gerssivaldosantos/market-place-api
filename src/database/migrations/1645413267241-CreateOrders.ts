f161c9e4fe6c08fe2b561b1837dea87342a43f81
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-21 00:27:39 -0300

    feat: create orders

---
+++ src/database/migrations/1645413267241-CreateOrders.ts
@@ -0,0 +1,90 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateOrders1645413267241 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.createTable(
+            new Table({
+                name: 'orders',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'customer_id',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'product_id',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'quantity',
+                        type: 'int',
+                        isNullable: false
+                    },
+                    {
+                        name: 'paid',
+                        type: 'boolean',
+                        isNullable: false,
+                        default: false
+                    },
+                    {
+                        name:'active',
+                        type: 'boolean',
+                        isNullable: false,
+                        default: true
+
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                        isNullable: false
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'desactived_at',
+                        type: 'timestamp',
+                        isNullable: true
+                    },
+                    {
+                        name: 'paid_at',
+                        type: 'timestamp',
+                        isNullable: true,
+                        default: null
+                    }
+
+                ],
+                foreignKeys: [
+                    {
+                        name: 'fk_customer',
+                        columnNames: ['customer_id'],
+                        referencedTableName: 'users',
+                        referencedColumnNames: ['id'],
+                    },
+                    {
+                        name: 'fk_product',
+                        columnNames: ['product_id'],
+                        referencedTableName: 'products',
+                        referencedColumnNames: ['id'],
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable('orders');
+    }
+
+}
