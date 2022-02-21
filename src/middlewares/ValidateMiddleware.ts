3286e683cb3ede10c7f3a2d37449ee94ebe52045
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-21 13:55:27 -0300

    feat: remove unused method

--- src/middlewares/ValidateMiddleware.ts
+++ src/middlewares/ValidateMiddleware.ts
@@ -5,26 +5,6 @@ import { User } from '../entities/UserEntity';
 import transport from '../config/main.init';
 
 class ValidateMiddleware {
-    async validateSyntax(req: Request, res: Response, next: NextFunction) {
-        try {
-            const { email, password } = req.body;
-            let user = new (User);
-            user.email = email;
-            user.password = password;
-            const errors = await validate(user);
-            if (errors.length > 0) {
-                return res.status(400).json({
-                    error: "email or password is invalid",
-                })
-            }
-            return next();
-        }
-        catch (err) {
-            return res.status(500).json({
-                error: "Internal server error"
-            })
-        }
-    }
 
     async validateEmail(req: Request, res: Response, next: NextFunction) {
         try {
