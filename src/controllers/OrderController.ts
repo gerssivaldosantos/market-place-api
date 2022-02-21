c67cf79c4640c4aa5fae323802e8209be7fd68b3
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-21 13:14:41 -0300

    feat: create order

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -3,6 +3,12 @@ import OrderService from '../services/OrderService';
 
 class OrderController {
 
+    async create(req: Request, res: Response) {
+        const {product_id, customer_id, seller_id, quantity} = req.body;
+        const result = await OrderService.create({product_id, customer_id, seller_id, quantity});
+        return res.status(result.status).json(result)
+    }
+
     async getAll(req: Request, res: Response) {
         const result = await OrderService.getAll();
         return res.status(result.status).json(result)
