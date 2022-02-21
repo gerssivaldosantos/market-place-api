69cb228993b2921e946511f97f441e2822e1884c
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-21 16:13:27 -0300

    feat: cors implement

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import express from "express";
 import routes from "./routes";
 import "./database/connection";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes)
 
 app.listen(8085, () => console.log("Server started on port 8085"));
