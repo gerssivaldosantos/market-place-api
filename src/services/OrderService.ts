b6fcac6f247f59dccf300c064b7a09fddbc28c0e
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-21 13:15:08 -0300

    feat: create Order

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,12 +1,24 @@
 import { getRepository } from "typeorm";
 import { Order } from "../entities/OrderEntity";
 
-class OrderService{
+class OrderService {
 
-    async getAll(){
+    async create({ product_id, customer_id, seller_id, quantity }) {
+        const repository = getRepository(Order)
+        const order = repository.create({
+            product_id,
+            customer_id,
+            seller_id,
+            quantity
+        })
+        await repository.save(order);
+        return { status: 200, data: order }
+    }
+
+    async getAll() {
         const repository = getRepository(Order)
         const orders = await repository.find()
-        return {status: 200, data: orders}
+        return { status: 200, data: orders }
     }
 }
 
