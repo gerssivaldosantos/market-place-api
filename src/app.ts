c8939c54f1603bd9f8ac984fb5a7912fcac9db04
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-24 15:42:43 -0300

    feat: app encapsulated and separated of listen to use in jest

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import express from "express";
+import routes from "./routes";
+import "./database/connection";
+import cors from "cors";
+import { Express } from 'express-serve-static-core';
+
+class MyApp {
+    app:Express;
+    constructor() {
+        this.app = express();
+        this.app.use(express.json());
+        this.app.use(cors());
+        this.app.use(routes)
+    }
+}
+
+export default new MyApp().app;
