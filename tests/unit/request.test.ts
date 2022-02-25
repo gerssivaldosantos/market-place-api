33926e5849694421eae8b570c9fc27a4606ddf5a
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-25 10:36:29 -0300

    feat: to test the structure of requests

---
+++ tests/unit/request.test.ts
@@ -0,0 +1,10 @@
+import { UserRequest } from '../../src/@types/UserRequest';
+
+describe('User Request', () => {
+
+    it('should return email syntax error', () => {
+        const user = new UserRequest();
+        
+    })
+
+})
