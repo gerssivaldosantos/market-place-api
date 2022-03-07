1b63fc64158d8b6fc890e6a907f253bb0d6abe84
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-03-07 09:55:51 -0300

    feat: conditional validation

--- src/@types/OrderRequest.ts
+++ src/@types/OrderRequest.ts
@@ -1,5 +1,16 @@
-import { Order } from "../entities/OrderEntity";
-
+import { IsNumber, IsUUID, ValidateIf } from 'class-validator'
 export class OrderRequest{
-    
+    @ValidateIf(request => request.customer_id !== undefined)
+    @IsUUID()
+    customer_id: string;
+    @ValidateIf(request => request.seller_id !== undefined)
+    @IsUUID()
+    seller_id: string;
+    @ValidateIf(request => request.product_id !== undefined)
+    @IsUUID()
+    product_id: string;
+    @ValidateIf(request => request.quantity !== undefined)
+    @IsNumber({allowInfinity: false, allowNaN: false, maxDecimalPlaces: 0})
+
+        
 }
