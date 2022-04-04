5dbb57c3d35313d9604501affd763e904185fa8e
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-04 00:55:24 -0300

    feat: add id creation

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,11 +1,14 @@
 import { getRepository } from "typeorm";
 import { Order } from "../entities/OrderEntity";
+import {v4 as uuid} from 'uuid';
 
 class OrderService {
 
     async create({ product_id, customer_id, seller_id, quantity }) {
+        const id = uuid();
         const repository = getRepository(Order)
         const order = repository.create({
+            id,
             product_id,
             customer_id,
             seller_id,
