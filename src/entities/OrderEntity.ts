58715805bbd69e38d6192ff0b3a82a15a95fc4d9
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-04 23:51:14 -0300

    feat: add relations to order

--- src/entities/OrderEntity.ts
+++ src/entities/OrderEntity.ts
@@ -1,27 +1,56 @@
-import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn} from "typeorm";
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn, OneToOne, JoinColumn, BeforeUpdate} from "typeorm";
+import { Product } from "./ProductEntity";
+import { User } from "./UserEntity";
 
 @Entity('orders')
 export class Order{
     @PrimaryColumn()
     id: string;
+
     @Column()
     customer_id: string;
+
     @Column()
     seller_id: string;
+
     @Column()
     product_id: string;
+
+    @OneToOne(()=> User)
+    @JoinColumn({name:"customer_id"})
+    customer: User;
+
+    @OneToOne(()=> User)
+    @JoinColumn({name:"seller_id"})
+    seller: User;
+
+    @OneToOne(()=> Product)
+    @JoinColumn({name:"product_id"})
+    product: Product;
+
     @Column()
     quantity: number;
+
     @Column()
     paid: boolean;
+
     @Column()
     active: boolean;
+
     @CreateDateColumn()
     created_at: Date;
+
     @UpdateDateColumn()
     updated_at: Date;
+
     @Column()
     desactived_at: Date;
+
     @Column()
     paid_at: Date;
+
+    @BeforeUpdate()
+    async update(){
+        this.updated_at = new Date();
+    }
 }
