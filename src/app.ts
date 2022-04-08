12ace63521077af8a3e80668161fd5be03fb897c
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-07 21:37:35 -0300

    feat: add swagger express structure

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,16 @@ import routes from "./routes";
 import "./database/connection";
 import cors from "cors";
 import { Express } from 'express-serve-static-core';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 class MyApp {
     app:Express;
     constructor() {
         this.app = express();
         this.app.use(express.json());
         this.app.use(cors());
-        this.app.use("/api/v1", routes)
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+        this.app.use('/api/v1', routes)
     }
 }
 
