345800c72e474c5f6e2c449c7b8cce872e729a0f
gerssivaldosantos <canalandows@gmail.com>
2022-05-15 12:55:32 -0300

    feat: enchance the console.log() in start server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(process.env.PORT  || 8085, () => {console.log("Server started on port 8085")});
+app.listen(process.env.PORT  || 8085, () => {console.log(`Server started on port ${process.env.PORT ?? 8085}`)});
